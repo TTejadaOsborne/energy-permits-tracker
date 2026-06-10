0306246e32d7c191ed5927a535b69bdd29dadaf7
Tomás Tejada <tomitejada@gmail.com>
2026-06-10 13:16:13 +0200

    Auto-update generate_sets_history.py 2026-06-10 13:16

--- generate_sets_history.py
+++ generate_sets_history.py
@@ -87,6 +87,7 @@ def extract_ree(ws, year, mes):
         _gen_disp = to_float(row[44]) if len(row) > 44 else None
         _gen_tram = to_float(row[40])
         _cd = to_float(row[7])
+        _gen_RdD = to_float(row[67]) if len(row) > 67 else None  # columna BP (índice 67)
         entries[key] = {
             "date":         lbl,
             "cap_gen":      to_float(row[4]),
@@ -100,7 +101,7 @@ def extract_ree(ws, year, mes):
             "cap_dem_disp": None,
             "cap_dem_neta": _cd,
             "acept":        to_float(row[10]),
-            "cap_gen_RdD":  None,
+            "cap_gen_RdD":  _gen_RdD,
         }
     return entries
 
